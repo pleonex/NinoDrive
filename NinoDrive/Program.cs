8fce497581d1922e2f1eab577b5033a4a1376784
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-02 19:48:38 +0100

    Get secret tokens from env vars

--- NinoDrive/Program.cs
+++ NinoDrive/Program.cs
@@ -8,36 +8,51 @@ namespace NinoDrive
 {
     public static class Program
     {
-        private const string ClientId = "YOUR_CLIENT_ID.apps.googleusercontent.com";
-        private const string ClientSecret = "ADD YOUR OWN CLIENT_SECRET";
+        private const string ClientId = ".apps.googleusercontent.com";
         private const string Scope = "https://spreadsheets.google.com/feeds https://docs.google.com/feeds";
         private const string RedirectUri = "urn:ietf:wg:oauth:2.0:oob";
 
         [STAThreadAttribute] // Needed to write the link to the clipboard
-        public static void Main(string[] args)
+        public static void Main()
         {
+            // Get the authorization
+            var authorization = GetAuthorization();
+
+            // Get the services and start working.
+            GoogleToXML go = new GoogleToXML("NinoDrive-v1", authorization);
+            go.PrepareService();
+            while (true)
+                go.FromSpreadsheet();
+        }
+
+        private static OAuth2Parameters GetAuthorization()
+        {
+            // Get the client ID and secret from environment variables for security.
+            var clientId = Environment.GetEnvironmentVariable("NINODRIVE_ID") + ClientId;
+            var clientSecret = Environment.GetEnvironmentVariable("NINODRIVE_SECRET");
+
             // Prepare the OAuth params.
             OAuth2Parameters oauthParams = new OAuth2Parameters();
-            oauthParams.ClientId = ClientId;
-            oauthParams.ClientSecret = ClientSecret;
+            oauthParams.ClientId = clientId;
+            oauthParams.ClientSecret = clientSecret;
             oauthParams.RedirectUri = RedirectUri;
             oauthParams.Scope = Scope;
 
             // Ask to get the authorization code. Give the URL and ask for the code.
             string authorizationUrl = OAuthUtil.CreateOAuth2AuthorizationUrl(oauthParams);
             Clipboard.SetText(authorizationUrl);
-            Console.WriteLine("A link has been copied to your clipboard. Please paste that"
-            + " link in a browser and authorize yourself on the webpage.  Once that is"
-            + " complete, type your access code and press enter to continue...");
+            Console.WriteLine("The following link has been copied to your clipboard.");
+            Console.WriteLine("Please paste that link in a browser and authorize");
+            Console.WriteLine("yourself on the webpage. Once that is complete, type");
+            Console.WriteLine("your access code and press Enter to continue...");
+            Console.WriteLine(authorizationUrl);
+
+            Console.Write("Access code: ");
             oauthParams.AccessCode = Console.ReadLine();
-
-            // Get the services and start working.
-            GoogleToXML go = new GoogleToXML("NinoDrive-v1", oauthParams);
-
             Console.WriteLine("-----------------------------------------");
-            go.PrepareService();
-            while (true)
-                go.FromSpreadsheet();
+            Console.WriteLine();
+
+            return oauthParams;
         }
     }
 }
