b2d6929f4ebc0239a7260b19c07995080eb9efe8
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-08 23:59:45 +0100

    CLI: Select spreadsheet if multiple results

--- NinoDrive/Program.cs
+++ NinoDrive/Program.cs
@@ -9,9 +9,6 @@ namespace NinoDrive
     {
         public static void Main()
         {
-            // Get the authorization
-            var authorization = AuthorizationManager.Instance;
-
             // Get the services and start working.
             var service = new SpreadsheetsService();
 
@@ -25,24 +22,39 @@ namespace NinoDrive
 
                 // Search for it
                 var spreadsheets = service.SearchSpreadsheets(title).ToArray();
-
-                // Present the result and if it's not an exact match confirm.
-                Console.WriteLine("\t{0} spreadsheets found:", spreadsheets.Length);
-                foreach (var sheet in spreadsheets)
-                    Console.WriteLine("\t* {0}", sheet.Title);
-
-                // If nothing found, continue
-                if (spreadsheets.Length == 0)
+                var targetSheet = SelectSpreadsheet(spreadsheets);
+                if (targetSheet == null)
                     continue;
 
-                // If it's not an exact match, ask if we should continue.
-                if (spreadsheets.Length > 1 || spreadsheets[0].Title != title) {
-                    Console.Write("The title doesn't match exactly. Continue? [Y/N] ");
-                    string reply = Console.ReadLine();
-                    if (reply.ToLower() != "y")
-                        continue;
-                }
+                // TODO: Convert.
             }
+        }
+
+        private static Spreadsheet SelectSpreadsheet(Spreadsheet[] spreadsheets)
+        {
+            // Present the result and if it's not an exact match confirm.
+            Console.WriteLine("{0} spreadsheets found:", spreadsheets.Length);
+            for (int i = 0; i < spreadsheets.Length; i++)
+                Console.WriteLine("\t{0}. {1}", i, spreadsheets[i].Title);
+
+            // If nothing found, continue
+            if (spreadsheets.Length == 0)
+                return null;
+
+            // If just a result, returns it
+            if (spreadsheets.Length == 1)
+                return spreadsheets[0];
+
+            // If it's not an exact match, ask if we should continue.
+            Console.Write("The title doesn't match exactly. Continue? [Y/n] ");
+            string reply = Console.ReadLine();
+            if (reply.ToLower() == "n")
+                return null;
+
+            // And ask for the index.
+            Console.Write("Type the number of the spreadsheet in the list to select: ");
+            int idx = Convert.ToInt32(Console.ReadLine());
+            return (idx < 0 || idx >= spreadsheets.Length) ? null : spreadsheets[idx];
         }
     }
 }
