e7c4d0f7041cb1f2564436ae5261117978d62c93
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-10 04:30:48 +0100

    Move into plugin folder

--- NinoDrive/Plugin/SpreadsheetGame.cs
+++ NinoDrive/Plugin/SpreadsheetGame.cs
@@ -23,10 +23,11 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-namespace NinoDrive.Converter
+namespace NinoDrive.Plugin
 {
     using System.Xml.Linq;
     using NinoDrive.Spreadsheets;
+    using NinoDrive.Converter;
     using Libgame;
     using Libgame.IO;
     using Mono.Addins;
